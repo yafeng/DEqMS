e1da67f8c781ff663b9bdbf001834b681a4e9311
Yafeng Zhu <yafeng.zhu@scilifelab.se>
2018-09-11 15:40:59 +0200

    prepare for ExperimentHub

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,18 +1,21 @@
-## validate with `AnnotationHub::readMetadataFromCsv("miRProteomicsData")`
+## validate with `AnnotationHub::readMetadataFromCsv("DEqMS")`
 ## (above pkg directory)
 
 main.data <- data.frame(
     Title = c("microRNA treated A431 cell proteomics data"),
     Description = c("High resolution isoelectric focusing LC-MS/MS data for A431 cells treated by three different microRNA mimics"),
-    RDataPath = c("miRProteomicsData/PXD004163.rds"),
-    BiocVersion="3.7",
+    RDataPath = c("DEqMS/PXD004163.rds"),
+    BiocVersion="3.8",
     SourceType="text",
+    Genome = "hg38",
+    SourceVersion = "b3046142103a2033da2137086b5afd36",
+    Coordinate_1_based = TRUE,
     SourceUrl="ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2016/06/PXD004163/Yan_miR_PSM_table.flatmzidtsv.txt",
     Species="Homo Sapiens",
     TaxonomyId="9606",
     DataProvider="Swedish BioMS infrastructure",
     Maintainer="Yafeng Zhu <yafeng.zhu@ki.se>",
-    RDataClass="character",
+    RDataClass="data.frame",
     DispatchClass="Rds",
     stringsAsFactors = FALSE
 )
