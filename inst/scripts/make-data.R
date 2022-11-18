e2e4a8cae2b377eca138daf83230733aeff048bc
Yafeng Zhu <yafengzhu@MacBook-Pro.local>
2022-11-18 16:29:40 +0800

    update make-data

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,9 +1,9 @@
 # Downloading the data.
 
-url1 <- "ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2016/06/PXD004163/Yan_miR_PSM_table.flatmzidtsv.txt"
+url1 <- "https://ftp.ebi.ac.uk/pride-archive/2016/06/PXD004163/Yan_miR_PSM_table.flatmzidtsv.txt"
 download.file(url1, destfile = "./miR_PSMtable.txt",method = "auto")
 
-url2 <- "ftp://ftp.pride.ebi.ac.uk/pride/data/archive/2016/06/PXD004163/Yan_miR_Protein_table.flatprottable.txt"
+url2 <- "https://ftp.ebi.ac.uk/pride-archive/2016/06/PXD004163/Yan_miR_Protein_table.flatprottable.txt"
 download.file(url2, destfile = "./miR_Proteintable.txt",method = "auto")
 
 
@@ -28,3 +28,19 @@ df.psm.filter = na.omit(df.psm.filter)
 colnames(df.psm.filter )[2]  = "gene"
 
 saveRDS(df.psm.filter, "PXD004163.rds")
+save(df.prot,file = "miR_Proteins.rda")
+
+url3 <- "https://ftp.ebi.ac.uk/pride-archive/2014/09/PXD000279/proteomebenchmark.zip"
+download.file(url3, destfile = "./PXD000279.zip",method = "auto")
+unzip("PXD000279.zip")
+df.prot3 = read.table("proteinGroups.txt",header=T,sep="\t",stringsAsFactors = F,
+                     comment.char = "",quote ="")
+save(df.prot3,file = "LFQ_benchmark.rda")
+
+
+url4 <- "ftp://massive.ucsd.edu/MSV000087597//quant/RD139_Software_Exports/Spectronaut_Mixed_Fasta_Export.csv"
+download.file(url2, destfile = "Spectronaut_Mixed_Fasta_Export.csv",method = "auto")
+
+df.dia = read.csv("Spectronaut_Mixed_Fasta.txt",row.names=1,sep = "\t")
+save(df.dia,file = "UPS1-spike-in.rda")
+
